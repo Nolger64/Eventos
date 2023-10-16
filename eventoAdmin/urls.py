b618e538a225f975746bcfe3d53f042bb67fc498
Nolger Luis Rodriguez Medina <nolgerluisrodriguezmedina@gmail.com>
2023-10-16 16:18:48 -0500

    16/10/2023 Agregue estilos al resto

--- eventoAdmin/urls.py
+++ eventoAdmin/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.lista_eventos, name='lista_eventos'),
     path('agregar/', views.agregar_evento, name='agregar_evento'),
-    path('eventos/eliminar/<int:evento_id>/', views.eliminar_evento, name='eliminar_evento'),
-    path('eventos/detalle/<int:evento_id>/', views.detalle_evento, name='detalle_evento'),
-    path('eventos/editar/<int:evento_id>/', views.editar_evento, name='editar_evento'),
+    path('eliminar/<int:evento_id>/', views.eliminar_evento, name='eliminar_evento'),
+    path('detalle/<int:evento_id>/', views.detalle_evento, name='detalle_evento'),
+    path('editar/<int:evento_id>/', views.editar_evento, name='editar_evento'),
 ]
