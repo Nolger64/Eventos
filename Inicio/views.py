96e4ca2fc4259dd12087e27e10eb9bea3baa8794
Nolger Luis Rodriguez Medina <nolgerluisrodriguezmedina@gmail.com>
2023-10-17 09:32:53 -0500

    17/10/2023 Arreglos en la pagina de incio

--- Inicio/views.py
+++ Inicio/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from eventoAdmin.models import Evento
 
 def base(request):
     return render(request, 'base.html')
 
 def inicio(request):
-    return render(request, 'inicio.html')
+    eventos = Evento.objects.all() # Paso 1: Obtener todos los eventos de la base de datos
+    totalReg = Evento.objects.latest('id') # Obtén el último registro en la tabla
+    ultimo_id = totalReg.id # Accede al ID del último registro
+    return render(request, 'inicio.html', {'eventos': eventos, 'total' : ultimo_id})
 
