f9254d1986d1b9495a804e7e47cd1829f0a59253
Nolger Rodriguez <nolgerluisrodriguezmedina@gmail.com>
2023-10-24 17:21:19 -0700

    24/10/2023 Cree la app Registro y cambie algo de diseño

--- Evento/urls.py
+++ Evento/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Inicio.urls')),
     path('eventos/', include('eventoAdmin.urls')),
+    path('registro', include('registro.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
